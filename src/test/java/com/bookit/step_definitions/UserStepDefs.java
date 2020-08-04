58fc19d45119fd06726cc012948b7ffcc5837933
“mustafanihat” <nihatirice@gmail.com>
2020-08-04 09:35:40 +0100

    update

--- src/test/java/com/bookit/step_definitions/UserStepDefs.java
+++ src/test/java/com/bookit/step_definitions/UserStepDefs.java
@@ -1,29 +1,32 @@
 package com.bookit.step_definitions;
 
 import com.bookit.utilities.BookItApiUtils;
+import com.bookit.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import static io.restassured.RestAssured.*;
 
 public class UserStepDefs {
     String token;
+    Response response;
 
     @Given("I logged Bookit api using {string} and {string}")
     public void i_logged_Bookit_api_using_and(String string, String string2) {
         token = BookItApiUtils.generateToken(string, string2);
-        System.out.println(token);
-
     }
 
     @When("I get the current user information from api")
     public void i_get_the_current_user_information_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        response = given().header("Authorization", token)
+                .when().get(ConfigurationReader.get("qa1api.uri") + "/api/users/me");
     }
 
     @Then("status code should be {int}")
     public void status_code_should_be(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        Assert.assertEquals(response.getStatusCode(),200);
     }
 }
