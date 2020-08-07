d312d115ebe227ccc447af6f1f904c5a908877fb
baranmmm <m.barann84@gmail.com>
2020-08-07 16:04:46 +0100

    update baran

--- src/test/java/com/bookit/step_definitions/UserStepDefs.java
+++ src/test/java/com/bookit/step_definitions/UserStepDefs.java
@@ -1,27 +1,44 @@
 package com.bookit.step_definitions;
 
 import com.bookit.utilities.BookItApiUtils;
+import com.bookit.utilities.ConfigurationReader;
+import com.bookit.utilities.DBUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import static io.restassured.RestAssured.*;
 
 public class UserStepDefs {
     String token;
-
+    Response response;
 
 
     @Given("I logged Bookit api using {string} and {string}")
     public void i_logged_Bookit_api_using_and(String email, String password) {
         token = BookItApiUtils.generateToken(email, password);
+
     }
 
     @When("I get the current user information from api")
     public void i_get_the_current_user_information_from_api() {
+        response = given().header("Authorization", token)
+                .get(ConfigurationReader.get("qa1api.uri") + "/api/users/me");
+
 
     }
 
     @Then("status code should be {int}")
-    public void status_code_should_be(Integer int1) {
+    public void status_code_should_be(int int1) {
+        Assert.assertEquals(response.statusCode(), int1);
+
+    }
+
+    @Then("the information about current user from api and database should be match")
+    public void the_information_about_current_user_from_api_and_database_should_be_match() {
+
 
     }
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -1,13 +1,13 @@
 @smoke
 Feature: User Verification
 
-@wip
+
   Scenario: verify information about logged user
     Given I logged Bookit api using "sbirdbj@fc2.com" and "asenorval"
-#    When I get the current user information from api
-#    Then status code should be 200
+    When I get the current user information from api
+    Then status code should be 200
 
-  @db
+  @wip @db
   Scenario: verify information about logged user from api and database
     Given I logged Bookit api using "sbirdbj@fc2.com" and "asenorval"
     When I get the current user information from api
