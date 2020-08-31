7fc1868c562ef30b57f56f63b5ad31804e8a4c16
“mustafanihat” <nihatirice@gmail.com>
2020-08-31 14:31:49 +0100

    update

--- src/test/java/com/bookit/pages/HuntPage.java
+++ src/test/java/com/bookit/pages/HuntPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.FindBy;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class HuntPage extends TopNavigationBar{
 
@@ -18,20 +19,27 @@ public class HuntPage extends TopNavigationBar{
     public void selectTomorrow(){
 
         Date dt = new Date();
+        Date today = new Date();
         Calendar c = Calendar.getInstance();
         c.setTime(dt);
-        c.add(Calendar.DATE, 1);
+        Random r = new Random();
+        int i = r.nextInt(6)+1;
+        c.add(Calendar.DATE, i);
         dt = c.getTime();
+        System.out.println(i);
+        System.out.println(dt);
+        System.out.println(today);
         BrowserUtils.waitForClickablility(calendarButton,5);
         calendarButton.click();
         BrowserUtils.waitFor(1);
 
-        if(dt.getDate()==1){
+        if(dt.getDate()<today.getDate()){
             Driver.get().findElement(By.xpath("//button[@aria-label='Next month']")).click();
             Driver.get().findElement(By.xpath("//td[.='"+dt.getDate()+"']")).click();
         }else{
             Driver.get().findElement(By.xpath("//td[.='"+dt.getDate()+"']")).click();
         }
+
         BrowserUtils.waitFor(2);
     }
 
